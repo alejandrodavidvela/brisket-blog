fb2ffca5bc2aa2ac438bebd1e37c93ceb706b576
alejandrodavidvela <alex.d.vela@gmail.com>
2021-02-16 19:14:43 -0600

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  siteMetadata: {
+    title: "Brisket Blog",
+  },
+  plugins: [
+    {
+      resolve: "gatsby-plugin-google-analytics",
+      options: {
+        trackingId: "G-3NFCHY4BV2",
+      },
+    },
+  ],
+};
